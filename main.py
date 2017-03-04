612d28cb977dfad5d5dae15ce2b873621fa01233
JackTheNerd <info@jackthenerd.com>
2017-03-04 19:03:36 +0000

    Added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def pass_gen(size, chars=string.ascii_lowercase + string.digits):
+	return "".join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(size))
+
+versionNumber = "0.0.1"
+
+print("CryptWord Version {}".format(versionNumber))
+
+passwordLength = int(input("How many characters do you want your password to be? "))
+
+print(pass_gen(passwordLength))
